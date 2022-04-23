b6ebc7a166ac99f4156836a385c67a0114d4734f
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-23 17:48:57 +0200

    added documentation to View Class

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -10,6 +10,15 @@ Notes:
 export default class View {
   _data;
 
+  // DOCUMENTATION
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Abdelrahman Soltan
+   */
   render(data, render = true) {
     // Gaurd claus : if (no data) or (data is an empty array) => render error message from error-function
     if (!data || (Array.isArray(data) && data.length === 0))
